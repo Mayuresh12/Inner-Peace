f70ac30c59d13a992b398860f7dbd9a00616559f
MRAO0212 <mayureshvrao@gmail.com>
2021-04-26 09:44:49 -0700

    improving design

--- Inner Peace/Inner Peace/ViewController.swift
+++ Inner Peace/Inner Peace/ViewController.swift
@@ -19,6 +19,62 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func updateQuote() {
+        guard let backgroundImageName = images.randomElement() else {
+            fatalError("Unable to read an image.")
+            
+        }
+        
+        background.image = UIImage(named: backgroundImageName)
+        
+        guard let selectedQuote = quote.randomElement() else {
+            fatalError("Unable to read a quote.")
+        }
+        
+        let drawBounds = quotes.bounds.inset(by: UIEdgeInsets(top: 250, left: 250, bottom: 250, right: 250))
+
+        var quoteRect = CGRect(x: 0, y: 0, width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        var fontSize: CGFloat = 120
+        var font: UIFont!
+
+        var attrs: [NSAttributedString.Key: Any]!
+        var str: NSAttributedString!
+
+        while true {
+            
+            font = UIFont(name: "Georgia-Italic", size: fontSize)!
+            attrs = [.font: font, .foregroundColor: UIColor.white]
+            str = NSAttributedString(string: selectedQuote.text, attributes: attrs)
+            quoteRect = str.boundingRect(with: CGSize(width: drawBounds.width, height: CGFloat.greatestFiniteMagnitude), options: .usesLineFragmentOrigin, context: nil)
+
+               if quoteRect.height > drawBounds.height {
+                    fontSize -= 4
+                } else {
+                    break
+                }
+
+        }
+        let format = UIGraphicsImageRendererFormat()
+        format.opaque = false
+        let renderer = UIGraphicsImageRenderer(bounds: quoteRect.insetBy(dx: -30, dy: -30), format: format)
+        quotes.image = renderer.image { ctx in
+            for i in 1...5 {
+                ctx.cgContext.setShadow(offset: .zero, blur: CGFloat(i * 2), color: UIColor.black.cgColor)
+                str.draw(in: quoteRect)
+            }
+            str.draw(in: quoteRect)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateQuote()
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        updateQuote()
+    }
+    
 }
 
 extension Bundle{
